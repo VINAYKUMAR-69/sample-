25e2d02673a64a44a2bda804e8ec9d79098ac6bd
G VINAY KUMAR <vinaykumarganga19@gmail.com>
2025-12-01 11:11:54 +0530

    Implement async login function in script.js

---
+++ frontend/script.js
@@ -0,0 +1,17 @@
+async function login() {
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+
+const response = await fetch("http://localhost:5000/api/login", {
+method: "POST",
+headers: {
+"Content-Type": "application/json"
+},
+body: JSON.stringify({ username, password })
+});
+
+
+const data = await response.json();
+document.getElementById("msg").innerText = data.message;
+}
